732534c124c5fc89e413268a8b60117b789c2d91
Georgios Tsimplis <georgetsiblis@gmail.com>
2023-08-15 07:22:41 +0200

    exception add

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+import os
 
 application=Flask(__name__)
 
@@ -20,32 +21,38 @@ def predict_datapoint():
     if request.method=='GET':
         return render_template('modelpage.html')
     else:
-        data=CustomData(
+        try:
+            data=CustomData(
 
-            city=request.form.get('city'),
-            room_type=request.form.get('Room_Type'),
-            person_capacity =int(request.form.get('person_capacity')),
-            multiple_rooms = int(request.form.get('Multiple_Rooms')),
-            business = int(request.form.get("bussiness")),
-            cleanliness_rating = int(request.form.get("Cleanliness_Rating")),
-            guest_satisfaction = int(request.form.get("Guest_Satisfaction")),
-            bedrooms = int(request.form.get("Bedrooms")),
-            city_center_km = float(request.form.get("City_Center_(km)")),
-            metro_distance_km = float(request.form.get("Metro_Distance_(km)"))
+                city=request.form.get('city'),
+                room_type=request.form.get('Room_Type'),
+                person_capacity =int(request.form.get('person_capacity')),
+                multiple_rooms = int(request.form.get('Multiple_Rooms')),
+                business = int(request.form.get("bussiness")),
+                cleanliness_rating = int(request.form.get("Cleanliness_Rating")),
+                guest_satisfaction = int(request.form.get("Guest_Satisfaction")),
+                bedrooms = int(request.form.get("Bedrooms")),
+                city_center_km = float(request.form.get("City_Center_(km)")),
+                metro_distance_km = float(request.form.get("Metro_Distance_(km)"))
 
-        )
-
-
-        pred_df=data.get_data_as_data_frame()
-        print(pred_df.head())
-        print("Before Prediction")
+            )
 
 
-        predict_pipeline=PredictPipeline()
-        print("Mid Prediction")
-        results=predict_pipeline.predict(pred_df)
-        print("after Prediction")
-        return render_template('modelpage.html',results=round(results[0],2))
+            pred_df=data.get_data_as_data_frame()
+            print(pred_df.head())
+            print("Before Prediction")
+
+
+            predict_pipeline=PredictPipeline()
+            print("Mid Prediction")
+            results=predict_pipeline.predict(pred_df)
+            print("after Prediction")
+            return render_template('modelpage.html',results=round(results[0],2))
+        
+        except Exception as e:
+            # Log the exception details for debugging
+            print("An error occurred:", str(e))
+            return "An error occurred while processing your request.", 500
     
 
 if __name__=="__main__":
